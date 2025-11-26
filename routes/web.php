2f3b42605d6eb5cebcf8e3fe0d941dc5ac3d2945
Ahmed Ibrahim <ai239128@gmail.com>
2025-11-26 09:59:59 +0200

    first init

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\jobController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('job', [jobController::class,'index']);
+
